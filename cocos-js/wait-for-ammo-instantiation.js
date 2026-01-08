42b2010e5326b2ffeba00f38055c0e146b1c8b9a
hyle <hoaile2k.dev@gmail.com>
2026-01-08 22:54:05 +0700

    update

--- cocos-js/wait-for-ammo-instantiation.js
+++
@@ -1,1 +0,0 @@
-System.register(["./instantiated-4c3917c1.js"],(function(t){"use strict";return{setters:[function(e){t("default",e.i9)}],execute:function(){}}}));
---
+++ src/settings.5608f.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scenes/start.fire",orientation:"",jsList:[],bundleVers:{internal:"d17e2",resources:"fb1fc",main:"5abc3"}};
